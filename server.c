d545777ecc00aa81d69b92129e883eef041960e9
angalinux <ciroangarella@gmail.com>
2024-01-24 12:09:08 +0100

    ho solo aggiunto un pò di commenti al codice del server

--- server.c
+++ server.c
@@ -11,7 +11,7 @@
 
 int main() {
     int serverSocket, clientSockets[MAX_CLIENTS], maxClients = MAX_CLIENTS;
-    fd_set readSet, writeSet;
+    fd_set readSet, writeSet; //set for read and write of select.h lib
     char buffer[BUFFER_SIZE];
 
     // Create a socket
@@ -40,27 +40,36 @@ int main() {
 
     printf("Server listening on port 50000...\n");
 
-    // Initialize client sockets array
+    /* Initialize client sockets array,
+     can handle up to maxClients */
     for (int i = 0; i < maxClients; ++i) {
-        clientSockets[i] = 0;
+
+        //initilize the all the fd to 0, so no socket are associated to the array
+        clientSockets[i] = 0; 
     }
 
+    /* main loop */
     while (1) {
-        FD_ZERO(&readSet);
-        FD_ZERO(&writeSet);
-        FD_SET(serverSocket, &readSet);
+        FD_ZERO(&readSet);  //initialize the fd set for the read
+        FD_ZERO(&writeSet); //initialize the fd set for the write
+        FD_SET(serverSocket, &readSet); //add the serverSocket to the read set
 
+        /* track the number of active socket*/
         int maxSocket = serverSocket;
 
         // Add client sockets to the set
         for (int i = 0; i < maxClients; ++i) {
+            //store in clientSocket the value of clientSocket's array
             int clientSocket = clientSockets[i];
 
+            //if clientSocket is associated to the array clientSockets
             if (clientSocket > 0) {
-                FD_SET(clientSocket, &readSet);
+                //add the clientSocket in readSet and writeSet
+                FD_SET(clientSocket, &readSet); 
                 FD_SET(clientSocket, &writeSet);
             }
-
+            
+            //update the max value of clientSocket
             if (clientSocket > maxSocket) {
                 maxSocket = clientSocket;
             }
@@ -84,7 +93,9 @@ int main() {
 
             // Add the new connection to the list of clients
             for (int i = 0; i < maxClients; ++i) {
+                //check if clientSockets position is free
                 if (clientSockets[i] == 0) {
+                    //associate the finded position to the new client socket
                     clientSockets[i] = newSocket;
                     break;
                 }
@@ -92,50 +103,54 @@ int main() {
         }
 
         // Check for data to read or write on client sockets
-    for (int i = 0; i < maxClients; ++i) {
-    int clientSocket = clientSockets[i];
+        for (int i = 0; i < maxClients; ++i) {
+            int clientSocket = clientSockets[i]; //the clientSocket that use the service
 
-    if (clientSocket > 0) {     
-        if (FD_ISSET(clientSocket, &readSet) && FD_ISSET(clientSocket, &writeSet)) {
+            //if clientSocket is occuped
+            if (clientSocket > 0) {     
+                //if clientSocket is ready for read and write
+                if (FD_ISSET(clientSocket, &readSet) && FD_ISSET(clientSocket, &writeSet)) {
 
-            char buffer[BUFFER_SIZE];
-            //pulizia buffer
-            memset(buffer, 0, sizeof(buffer));
-            //lettura del socket
-            ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-            //aggiunta terminatore stringa
-            buffer[bytesRead] = '\0';
-            // printf("Contenuto del buffer ricevuto: %s\n", buffer); per il debug
+                    //main of the server's code...
+
+                    char buffer[BUFFER_SIZE];
+                    //clear of the buffer
+                    memset(buffer, 0, sizeof(buffer));
+                    //reading of socket
+                    ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+                    //aggiunta terminatore stringa
+                    buffer[bytesRead] = '\0';
+                    // printf("Contenuto del buffer ricevuto: %s\n", buffer); per il debug
 
 
-            if (bytesRead < 0) {
-                perror("Errore durante la ricezione dei dati");
-            } else if (bytesRead == 0) { //se non riceve dati dal client
-                // Connessione chiusa dal client
-                printf("Client disconnesso\n");
-            } else {
-                // se la lettura va a buon fine , gestione dei comandi FTP
-                printf("Ho ricevuto dal client: %s\n", buffer);
+                    if (bytesRead < 0) { //error
+                        perror("Errore durante la ricezione dei dati");
+                    } else if (bytesRead == 0) { //if not recv byte from the reading
+                        // close the client
+                        printf("Client disconnesso\n");
+                        close(clientSocket);
 
-                const char* hello = "hello";
-                int cmpResult = strcmp(buffer, hello); //controllo del comando
-                //printf("Risultato del confronto: %d\n", cmpResult); per il debug
+                    } else {
+                        // if the reading is right, handle the comand
+                        printf("Ho ricevuto dal client: %s\n", buffer);
 
-                if (cmpResult == 0) {
-                   // printf("Il comando è: %s\n", buffer); per il debug
+                        const char* hello = "hello";
+                        int cmpResult = strcmp(buffer, hello); //controllo del comando
+                        //printf("Risultato del confronto: %d\n", cmpResult); per il debug
 
-                    // Invia "hello world from server" al client
-                    const char* response = "hello world from server";
-                    send(clientSocket, response, strlen(response), 0);
-                    printf("Ho mandato al client: %s\n", response);
+                        if (cmpResult == 0) {
+                            // printf("Il comando è: %s\n", buffer); per il debug
+
+                            // Invia "hello world from server" al client
+                            const char* response = "hello world from server";
+                            send(clientSocket, response, strlen(response), 0);
+                            printf("Ho mandato al client: %s\n", response);
+                        }
+                    }
                 }
             }
         }
-    }
-}
 
-        
-        
     }
 
     return 0;
